625b6cd36a16cd4885819af8c25c929067cf5b1a
joao Santos <joaocarlosrh23@gmail.com>
2024-02-01 11:49:52 -0300

    edit

---
+++ assets/js/style-switcher.js
@@ -0,0 +1,26 @@
+/* toggle style switcher */
+const styleSwitcherToggle = document.querySelector(".style-switcher-toggler");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+});
+
+// hide style switcher on scroll
+window.addEventListener("scroll", () => {
+    const styleSwitcher = document.querySelector(".style-switcher");
+    if (styleSwitcher.classList.contains("open")) {
+        styleSwitcher.classList.remove("open");
+    }
+});
+
+/* theme colors */
+const alternateStyles = document.querySelectorAll(".alternate-style");
+
+function setActiveStyle(color) {
+    alternateStyles.forEach((style) => {
+        if (color === style.getAttribute("title")) {
+            style.removeAttribute("disabled");
+        } else {
+            style.setAttribute("disabled", "true");
+        }
+    });
+}
